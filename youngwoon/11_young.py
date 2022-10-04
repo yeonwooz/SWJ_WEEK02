5225198baed9256f43b88a9e6695ca6635098d0c
young970 <yyww9@naver.com>
2022-10-04 17:01:48 +0900

    second commit

--- youngwoon/11_young.py
+++ youngwoon/11_young.py
@@ -3,9 +3,35 @@ import sys
 n = int(input())
 
 stack = []
-
-# for _ in range(n):
-input = sys.stdin.readline
-commend = list(map(int, input().split()))
+answer = []
+for _ in range(n):
+    input = sys.stdin.readline
+    commend = list(input().split())
     
-print(commend)
+    if commend[0] == 'push':
+        stack.append(commend[1])
+    
+    elif commend[0] == 'pop':
+        if len(stack) != 0:
+            answer.append(stack[-1])
+            stack.pop()
+        else:
+            answer.append(-1)
+
+    elif commend[0] == 'size':
+        answer.append(len(stack))
+
+    elif commend[0] == 'empty':
+        if len(stack) == 0:
+            answer.append(1)
+        else:
+            answer.append(0)
+    
+    elif commend[0] == 'top':
+        if len(stack) != 0:
+            answer.append(stack[len(stack)-1])
+        else:
+            answer.append(-1)
+
+for i in range(len(answer)):
+    print(answer[i])
